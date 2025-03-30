73dded7fceab7752659b4e8ea08960f170e9b030
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-03-30 18:53:27 +0530

    Interview  is  updated.

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,6 +1,23 @@
-const InterviewPage = () => {
-    return (
-        <div>InterviewPage</div>
-      )
-    }
-export default InterviewPage
+import { getAssessments } from "@/actions/interview";
+import StatsCards from "./_components/stats-cards";
+import PerformanceChart from "./_components/performance-chart";
+import QuizList from "./_components/quiz-list";
+
+export default async function InterviewPage() {
+  const assessments = await getAssessments();
+
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
+}
